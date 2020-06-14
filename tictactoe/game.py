3c2d092e4fa6dd9216b0c0d711fd50a7c74ee1b1
chris@prime <66511731+aioooi@users.noreply.github.com>
2020-06-14 22:38:53 +0200

    game works roughly

--- tictactoe/game.py
+++ tictactoe/game.py
@@ -2,18 +2,19 @@
 
 import numpy as np
 
+
 class Game:
     # board values
     _COMPUTER = -1
     _EMPTY = 0
     _HUMAN = 1
-  
+
     # board shape
     _SHAPE = (3, 3)
-  
+
     def __init__(self):
         self._board = np.zeros(self._SHAPE, dtype=int)
-  
+
     def __str__(self):
         number = [
             "\u2070",
@@ -28,35 +29,77 @@ class Game:
             "\u2079"
         ]
         numpad = number[7:10] + number[4:7] + number[1:4]
-    
-        s = "\u2554" + ("\u2550" * 3 + "\u2564") * 2 + "\u2550" * 3 + "\u2557\n"
+
+        s = "\u2554" + ("\u2550" * 3 + "\u2564") * \
+            2 + "\u2550" * 3 + "\u2557\n"
         t = "\u2551" + " {} \u2502" * 2 + " {} \u2551\n"
-        q = "\u255f" + ("\u2500" * 3 + "\u253c") * 2 + "\u2500" * 3 + "\u2562\n"
-        v = "\u255a" + ("\u2550" * 3 + "\u2567") * 2 + "\u2550" * 3 + "\u255d\n"
-        
+        q = "\u255f" + ("\u2500" * 3 + "\u253c") * \
+            2 + "\u2500" * 3 + "\u2562\n"
+        v = "\u255a" + ("\u2550" * 3 + "\u2567") * \
+            2 + "\u2550" * 3 + "\u255d\n"
+
         ui = numpad
         # computer plays ☯
-        ui = ["\u262f" if _ == self._COMPUTER else n 
-                for n, _ in zip(ui, self._board.flatten())]
-        # human plays ྾ 
-        ui = ["\u0fbe" if _ == self._HUMAN else n 
-                for n, _ in zip(ui, self._board.flatten())]
-    
-        return (s + (t + q) * 2 + t + v).format(*ui)
+        ui = ["\u262f" if _ == self._COMPUTER else n
+              for n, _ in zip(ui, self._board.flatten())]
+        # human plays ྾
+        ui = ["\u0fbe" if _ == self._HUMAN else n
+              for n, _ in zip(ui, self._board.flatten())]
 
+        return (s + (t + q) * 2 + t + v).format(*ui)
 
     def play(self, first_move=1):
         if first_move != 1:
-            first = self._COMPUTER
-        
+            turn = self._COMPUTER
+        turn = self._HUMAN
+
+        print(self)
+
+        while np.any(self._board == self._EMPTY):
+            if turn == self._COMPUTER:
+                self.make_move()
+                turn = self._HUMAN
+            else:
+                numpad = [7, 8, 9, 4, 5, 6, 1, 2, 3]
+                empty_field_labels = [str(numpad.index(e + 1) + 1)
+                                      for e in np.where(self._board.flatten() == self._EMPTY)[0]]
+
+                f = numpad.index(int(input("Choose field {}: ".format(" ".join(
+                    empty_field_labels)))))
+                # if f not in empty:
+                #     # TODO
+                #     raise ValueError
+
+                self.enter_move((int(f / 3), f % 3))
+                turn = self._COMPUTER
+            
+            print(self)
+
+            check = [
+                bool(len(self._check_triplet(3 * self._COMPUTER))),
+                bool(len(self._check_triplet(3 * self._HUMAN))),
+                not np.any(self._board == self._EMPTY)
+            ]
+
+            if check[0]:
+                print("I win!")
+                break
+            if check[1]:
+                print("You win!")
+                break
+            if check[2] and not (check[0] or check[1]):
+                print("That's a draw!")
+                break
+                
 
 
     """
     Human makes a move
     """
-    def enter_move(self, move=(0,0)):
+
+    def enter_move(self, move=(0, 0)):
         assert(len(move) == 2)
-    
+
         if any([_ < 0 or _ > 2 for _ in move]):
             raise Exception("invalid move: position does not exist")
         else:
@@ -65,51 +108,58 @@ class Game:
             else:
                 self._board[move] = self._HUMAN
 
-
     """
     Computer makes a move
     """
+
     def make_move(self):
         # TODO checks etc.
 
         # random move
         # TODO connect with handicap
-        if np.any(self._board == _EMPTY):
-            # TODO return value?
-            return _random_move()
-        
+        # if np.any(self._board == self._EMPTY):
+        #     # TODO return value?
+        #     return self._random_move()
+
         action = [
-            _win
-            _avoid_defeat,
-            _matchball,
-            _center,
-            _opposite_corner,
-            _empty_corner,
-            _random_move,
+            self._win,
+            self._avoid_defeat,
+            self._matchball,
+            self._center,
+            self._opposite_corner,
+            self._empty_corner,
+            self._random_move,
         ]
 
         while len(action):
             a = action.pop(0)
+            print(a)
             if a():
                 break
 
         # TODO return value?
         return
 
+    """
+    Check whether triplet with l1 norm equal to value exists
+    """
 
-    def _win():
+    def _check_triplet(self, value):
         rows = np.dot(self._board, np.ones(3))
         cols = np.dot(np.ones(3), self._board)
         diag = self._board.trace()
-        sdiag = self._board[::-1].trace()
+        sdiag = self._board[:: -1].trace()
 
-        check = np.where(
-            np.concatenate((rows, cols, [diag], [sdiag])) == 2 * self._COMPUTER)
+        return np.where(
+            np.concatenate((rows, cols, [diag], [sdiag])) == value)[0]
 
-        if len(check[0]):
-            i = np.random.randint(len(check[0]))
+    def _win(self):
+        check = self._check_triplet(2 * self._COMPUTER)
+
+        if len(check):
+            i = np.random.randint(len(check))
             c = check[i]
-            
+
             if c in range(3):
                 # row
                 x = np.where(self._board[c, :] == self._EMPTY)[0][0]
@@ -124,26 +174,19 @@ class Game:
             else:
                 # c == 7, secondary diagonal
                 np.fill_diagonal(self._board[::-1], self._COMPUTER)
-            
+
             return True
-        
+
         else:
             return False
 
+    def _avoid_defeat(self):
+        check = self._check_triplet(2 * self._HUMAN)
 
-    def _avoid_defeat():
-        rows = np.dot(self._board, np.ones(3))
-        cols = np.dot(np.ones(3), self._board)
-        diag = self._board.trace()
-        sdiag = self._board[::-1].trace()
-
-        check = np.where(
-            np.concatenate((rows, cols, [diag], [sdiag])) == 2 * self._HUMAN)
-
-        if len(check[0]):
-            i = np.random.randint(len(check[0]))
+        if len(check):
+            i = np.random.randint(len(check))
             c = check[i]
-            
+
             if c in range(3):
                 # row
                 x = np.where(self._board[c, :] == self._EMPTY)[0][0]
@@ -155,36 +198,33 @@ class Game:
             elif c == 6:
                 # diagonal
                 d = np.array(np.diagonal(self._board))
-                x = np.where(d == self._EMPTY))[0][0]
+                x = np.where(d == self._EMPTY)[0][0]
                 d[x] = self._COMPUTER
                 np.fill_diagonal(self._board, d)
             else:
                 # c == 7, secondary diagonal
                 d = np.array(np.diagonal(self._board[::-1]))
-                x = np.where(d == self._EMPTY))[0][0]
+                x = np.where(d == self._EMPTY)[0][0]
                 d[x] = self._COMPUTER
                 np.fill_diagonal(self._board[::-1], d)
-            
+
             return True
-        
+
         else:
             return False
 
+    def _matchball(self):
+        return False
 
-    def _matchball():
-        pass
-
-
-    def _center():
-        if self._board[1,1] == self._EMPTY:
-            self._board[1,1] = self._COMPUTER
+    def _center(self):
+        if self._board[1, 1] == self._EMPTY:
+            self._board[1, 1] = self._COMPUTER
             return True
         else:
             return False
 
-
-    def _opposite_corner():
-        corner = [(0,0), (0,2), (2,0), (2,2)]
+    def _opposite_corner(self):
+        corner = [(0, 0), (0, 2), (2, 0), (2, 2)]
 
         if self._board[corner[0]] + self._board[corner[3]] == self._HUMAN:
             if self._board[corner[0]] == self._EMPTY:
@@ -201,18 +241,16 @@ class Game:
         else:
             return False
 
-
-    def _empty_corner():
-        corner = [(0,0), (0,2), (2,0), (2,2)]
-        empty = np.where(self._board.flatten()[[0,2,6,8]] == self._EMPTY)
+    def _empty_corner(self):
+        corner = [(0, 0), (0, 2), (2, 0), (2, 2)]
+        empty = np.where(self._board.flatten()[[0, 2, 6, 8]] == self._EMPTY)
         if len(empty[0]):
             self._board[corner[np.random.randint(len(empty))]] = self._COMPUTER
             return True
         else:
             return False
 
-
-    def _random_move():
+    def _random_move(self):
         empty = np.where(self._board == self._EMPTY)
         if len(empty[0]):
             i = np.random.randint(len(empty[0]))
@@ -220,3 +258,7 @@ class Game:
             return True
         else:
             return False
+
+
+g = Game()
+g.play()
